d247347af45573985d527721053f92c7bed31e66
3A532077 <k0982637445@gmail.com>
2018-10-09 19:21:36 +0800

    載入HelloWorld

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17492
+ * Date: 2018/10/9
+ * Time: 下午 07:19
+ */
+
+namespace Demo;
+
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World ~'.PHP_EOL;
+    }
+
+}
