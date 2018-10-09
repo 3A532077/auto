79a5618ce7e3bea6f6db25b7265478311997dcab
3A532077 <k0982637445@gmail.com>
2018-10-09 19:24:57 +0800

    載入Lara & Someone

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17492
+ * Date: 2018/10/9
+ * Time: 下午 07:22
+ */
+
+namespace Demo\Hello;
+
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+
+}
---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 17492
+ * Date: 2018/10/9
+ * Time: 下午 07:23
+ */
+
+namespace Demo\Hello;
+
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+
+}
